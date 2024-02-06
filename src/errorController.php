76233fc84acd001d52e05fc5ebe2da5cac1cd09d
Givinghawk <giveinghawk@gmail.com>
2024-02-06 14:49:48 +0000

    Updates to errorController

--- src/errorController.php
+++ src/errorController.php
@@ -12,8 +12,9 @@ class ErrorController
             $log = fopen(__DIR__ . '/../application_error.log', 'a');
             fwrite($log, '[' . date('Y-m-d H:i:s') . '] ' . $errstr . ' in ' . $errfile . ' on line ' . $errline . PHP_EOL);
             fclose($log);
-            $fullstr = "Aw shucks your thingy doesn't work, contact the developer :(";
-            $this->throwError($fullstr);
+            // Error string reccomended by @Freezemage0
+            $errorString = "Aw shucks your thingy doesn't work, contact the developer :(";
+            $this->throwError($errorString);
             die();
         });
 
@@ -21,8 +22,9 @@ class ErrorController
             $log = fopen(__DIR__ . '/../application_error.log', 'a');
             fwrite($log, '[' . date('Y-m-d H:i:s') . '] ' . $e->getMessage() . ' in ' . $e->getFile() . ' on line ' . $e->getLine() . PHP_EOL);
             fclose($log);
-            $fullstr = "Aw shucks your thingy doesn't work, contact the developer :(";
-            $this->throwError($fullstr);
+            // Error string reccomended by @Freezemage0
+            $errorString = "Aw shucks your thingy doesn't work, contact the developer :(";
+            $this->throwError($errorString);
             die();
         });
     }
@@ -30,7 +32,7 @@ class ErrorController
     public function throwError($error)
     {
         $error_message = $error;
-        echo '<html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Error</title><link rel="stylesheet" href="JHGuYNTFGytkum.css"></head><body class="bg-zinc-900 flex justify-center items-center"><div class="bg-zinc-800 rounded-xl p-5"><div class="text-center"><h1 class="text-6xl text-white">Error</h1><p class="text-red-500 text-xl">' . $error_message . '</p><p class="text-white"><a href="https://github.com/givinghawk/EasyServe" class="text-sky-500 font-semibold">GitHub</a> | <a href="https://github.com/givinghawk/EasyServe/wiki" class="text-sky-500 font-semibold">Documentation</a> | <a href="https://github.com/givinghawk/EasyServe/issues" class="text-sky-500 font-semibold">Report an issue</a></p></div></div></body></html>';
+        echo '<html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Error</title><link rel="stylesheet" href="JHGuYNTFGytkum.css"></head><body class="bg-zinc-900 flex justify-center items-center"><div class="bg-zinc-800 rounded-xl p-5"><div class="text-center"><h1 class="text-6xl text-white">Error</h1><p class="text-red-500 text-xl">' . $error_message . '</p><p class="text-white"><a href="https://github.com/givinghawk/EasyServe" class="text-sky-500 font-semibold">GitHub</a> | <a href="https://github.com/givinghawk/EasyServe/wiki" class="text-sky-500 font-semibold">Documentation</a> | <a href="https://github.com/givinghawk/EasyServe/issues" class="text-sky-500 font-semibold">Report an issue</a></p><p class="text-white font-semibold">EasyServe '.file_get_contents(__DIR__.'/../version.txt').'</p></div></div></body></html>';
         die();
     }
 
